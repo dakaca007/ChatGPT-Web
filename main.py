b3727d1dd8cc7f9062c5d52713e6b618e0fd6e48
dakaca007 <129284180+dakaca007@users.noreply.github.com>
2024-10-22 09:13:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,4 +22,4 @@ def handle_get_messages():
     emit('receive_message', {'messages': messages})
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=80)
+    socketio.run(app, host='0.0.0.0', port=80, use_reloader=True)
